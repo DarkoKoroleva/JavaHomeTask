146aee34e18f7d3f5a13d41faec2273a0d809ea1
Daria <dkoroleva2004@gmail.com>
2024-12-02 19:37:11 +0300

    finally

---
+++ src/exception/Finally.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class Finally {
+//try без catch и finally - нельзя, нечего выполнять при exception
+//try только с finally - можно, при вызове exception выполнится блок finally
+//try с двумя finally - нельзя, конструкция недопустима.
+}
